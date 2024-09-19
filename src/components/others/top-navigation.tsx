31d45c85d4151f54032692ca6db307660fd9b781
sujal shah <sujalharishshah@gmail.com>
2024-09-20 03:44:00 +0530

    add: github link added

--- src/components/others/top-navigation.tsx
+++ src/components/others/top-navigation.tsx
@@ -25,7 +25,7 @@ const TopNavigation = (props: Props) => {
           </button>
          </div>
         </Link>
-        <Link href={"https://github.com/ShahSujal"}>
+        <Link href={"https://github.com/ShahSujal/Stakeyards"}>
          <div className=" w-8 h-8 relative rounded-full ">
           <Image src="/content/cardbg.jpeg" width={100} height={100} alt="" className=" w-full h-full rounded-2xl absolute top-0 object-cover transition"/>
          <button className="flex items-center justify-center w-8 h-8 relative rounded-full text-gray-400 bg-transparent backdrop-blur-xl z-10 hover:text-white hover:backdrop-blur-sm transition duration-1000 ">
